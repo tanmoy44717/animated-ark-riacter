2db7245ff37ea32ed209777a757c9572c61504bd
tanmoy <majumdertanmoy510@gmail.com>
2026-02-09 00:49:06 +0530

    Updates

--- _next/static/7GqaLAWZTovdctV97-GKe/_buildManifest.js
+++ _next/static/7GqaLAWZTovdctV97-GKe/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/animated-ark-riacter//_next/:path+",
+        "destination": "/animated-ark-riacter/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
